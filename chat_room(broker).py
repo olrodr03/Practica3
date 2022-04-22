249197a26e74b70622ec2b4ed3a401707c78af88
nataga07 <98947401+nataga07@users.noreply.github.com>
2022-04-22 17:55:41 +0200

    Update chat_room(broker).py

--- chat_room(broker).py
+++ chat_room(broker).py
@@ -1,3 +1,43 @@
+"""
+ENTREGA 3 
+Chat Room - Broker
+
+Olga Rodríguez Acevedo
+Claudia Casado Poyatos
+Natalia García Domínguez
+
+"""
+def get_info(clients,usuario):
+    s = []
+    for i in clients.items():
+        if i[1]['nombre'] == usuario:
+            s = [usuario, i[1]['address'], i[1]['port'], i[1]['authkey']]
+    return s
+
+def serve_client(conn, pid, clients, usuarios):
+    connected = True
+    while connected:
+        try:
+            (m, s, p) = conn.recv()
+            if m == 'getlist':
+                all_clients_connected = get_all_clients_connected(clients)
+                conn.send(all_clients_connected)
+            elif m == 'connect':
+                info_s = get_info(clients, s)
+                if info_s != []:
+                    send_msg(info_s,p)
+                conn.send(info_s)
+            elif m == 'info del usuario que quiere hablar conmigo':
+                conn.send(s)
+            else:
+                send_msg_all(pid, m, clients)
+        except EOFError:
+            print('conexión interrumpida por el client')
+            connected = False
+    usuarios.remove(clients[pid]['nombre'])
+    del clients[pid]
+    send_msg_all(pid, f'Los usuarios conectados son: {usuarios}', clients)
+
 def main(ip_address, usuarios):
     with Listener(address = (ip_address, 6000), authkey = b'secret password server') as listener:
         print('listener starting')
