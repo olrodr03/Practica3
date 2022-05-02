f55946b194c34f61965b07941c70bd52948f1a48
olrodr03 <98947628+olrodr03@users.noreply.github.com>
2022-05-02 17:35:53 +0200

    Update chat_broker.py

--- chat_broker.py
+++ chat_broker.py
@@ -1,4 +1,11 @@
 """
+PROGRAMACIÓN DISTRIBUIDA
+------------------------
+Práctica 3: Chat
+
+Autoras: Claudia Casado Poyatos
+         Natalia García Domínguez
+         Olga Rodríguez Acevedo
 """
 
 from multiprocessing.connection import Listener, Client
@@ -6,18 +13,20 @@ import sys
 from multiprocessing import Process, Manager
 import traceback
 
-def send_msg_all(pid,msg,clients):
-  for client, client_info in clients.items():
-    print(f"enviando: '{msg}' a {client_info}")
-    with Client(address = (client_info['address'],client_info['port']), authkey=client_info['authkey']) as conn:
-      if not client ==pid:
-        conn.send((pid,msg))
-      else:
-        conn.send(f"mensaje '{msg}' procesado")
+def send_msg_all(pid, msg, clients):
+    for client, client_info in clients.items():
+      print(f"enviando: '{msg}' a {client_info}")
+      with Client(address = (client_info['address'], client_info['port']), authkey = client_info['authkey']) as conn:
+        if not client == pid:
+          conn.send((pid,msg))
+        else:
+           conn.send(f"mensaje '{msg}' procesado")
+
 def send_msg(info_receptor,info_emisor): #Para enviar un mensae al inicio de cada conversacion
   print(f"enviando mensaje a {info_receptor[0]}")
   with Client(addres=(info_receptor[1],info_receptor[2]), authkey=info_receptor[3]) as conn:
     conn.send((info_emisor, 'informacion del emisor'))
+
 def clients_connected(clients): #Esta funcion devuelve una lista con los nombres de las personas en linea
   list_clients_connected=[]
   for i in clients.items():
