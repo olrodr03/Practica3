2ba68726b6c151f97e64defc1db0e9af4cee304c
claucasa <98829329+claucasa@users.noreply.github.com>
2022-05-02 22:08:51 +0200

    Update chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -122,56 +122,57 @@ def connection(info_emisor, info_receptor, fn):
                 traceback.print_exc()
         print("**************Servidor cerrado**************")
          
-def main(server_address, info):
-    print("**************Intentando conectar**************")
-    with Client(address = (server_address, 6000), authkey = b'secret password server') as conn:
-        fn = sys.stdin.fileno()
-        cl = Process(target = client_listener, args = (info, conn, fn, ))
-        cl.start()
-        sleep(1)
-        conn.send(info)
-        connected = True
-        while connected:
-            command = input('Escribe "lista" si quieres conocer los usuarios en línea o \n "chat" si quieres iniciar una conversación \n')
-            if command == "lista":
-                conn.send((command, 0, 0))
-                list_clients_connected = conn.recv()
-                print("Los usuarios conectados son:", list_clients_connected)
-            elif command == "chat":
-                user = input('¿Con quién quieres hablar? ')
-                conn.send((command, user, info))
-                info_user = conn.recv()
-                if info_user == []:
-                    print("Nombre incorrecto. Vuelve a intentarlo.")
-                else:
-                    connected = False
-                    conn.close()
-                    cl.terminate()
-                    connection1 = Process(target = connection, args = (info, info_user, fn, ))
-                    connection1.start()
-            elif command == "salir":
-                connected = False
-                conn.close()
-            elif command == "ACEPTO":
-                info_client = conn.recv()
-                connected = False
-                conn.close()
-                cl.terminate()
-                connection_emisor = Process(target = emisor_connection, args = (info_client, fn, ))
-                connection_emisor.start()
-        cl.terminate()
-
-if __name__ == '__main__':
-    server_address = '192.168.9.6' # Dirección IP del 'broker'.
-    client_address = '' # Dirección IP del 'client'.
-    client_port = 6001
-              
-    nombre = input('¿Cuál es tu nombre? ')
-    if len(sys.argv) > 1:
-        client_port = int(sys.argv[1])
-    if len(sys.argv) > 2:
-        client_address= sys.argv[2]
-    if len(sys.argv) > 3:
-        server_address= sys.argv[3]
-    info = {'nombre': nombre, 'address': client_address, 'port': client_port, 'authkey': b'secret client server'}
-    main(server_address, info)             
+def main(server_address, info): 
+    print("**************Intentando conectar**************") 
+    with Client(address = (server_address, 6000), authkey = b'secret password server') as conn: 
+        fn = sys.stdin.fileno() 
+        cl = Process(target = client_listener, args = (info, conn, fn, )) 
+        cl.start() 
+        sleep(1) 
+        conn.send(info) 
+        connected = True 
+        while connected: 
+            command = input('Escribe "lista" si quieres conocer los usuarios en línea o \n "chat" si quieres iniciar una conversación \n') 
+            if command == "lista": 
+                conn.send((command, 0, 0)) 
+                list_clients_connected = conn.recv() 
+                print("Los usuarios conectados son:", list_clients_connected) 
+            elif command == "chat": 
+                user = input('¿Con quién quieres hablar? ') 
+                conn.send((command, user, info)) 
+                info_user = conn.recv() 
+                if info_user == []: 
+                    print("Nombre incorrecto. Vuelve a intentarlo.") 
+                else: 
+                    connected = False 
+                    conn.close() 
+                    cl.terminate() 
+                    connection1 = Process(target = connection, args = (info, info_user, fn, )) 
+                    connection1.start() 
+            elif command == "salir": 
+                connected = False 
+                conn.close() 
+            elif command == "ACEPTO": 
+                info_client = conn.recv() 
+                connected = False 
+                conn.close() 
+                cl.terminate() 
+                connection_emisor = Process(target = emisor_connection, args = (info_client, fn, )) 
+                connection_emisor.start() 
+        cl.terminate() 
+ 
+if __name__ == '__main__': 
+    server_address = '192.168.9.6' # Dirección IP del 'broker'. 
+    client_address = '' # Dirección IP del 'client'. 
+    client_port = 6001 
+               
+    nombre = input('¿Cuál es tu nombre? ') 
+    if len(sys.argv) > 1: 
+        client_port = int(sys.argv[1]) 
+    if len(sys.argv) > 2: 
+        client_address= sys.argv[2] 
+    if len(sys.argv) > 3: 
+        server_address= sys.argv[3] 
+    info = {'nombre': nombre, 'address': client_address, 'port': client_port, 'authkey': b'secret client server'} 
+    main(server_address, info)              
+ 
